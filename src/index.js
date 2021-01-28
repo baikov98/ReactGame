2598327545f06cedf95eca0502e4dbd1120d66ba
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-28 22:34:38 +0500

    jumpto func

--- src/index.js
+++ src/index.js
@@ -62,6 +62,12 @@ class Game extends React.Component {
       xIsNext: true
     }
   }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
+  }
   handleClick(i) {
     const history = this.state.history
     const current = history[history.length - 1]
