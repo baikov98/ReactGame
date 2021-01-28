e3cb524c88df730a5b6a7a4d4054ae62998607ef
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-28 22:43:21 +0500

    final

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ class Game extends React.Component {
   }
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
     const moves = history.map((step, move) => {
       const desc = move ? 'Перейти к ходу '+move : 'К началу игры'
